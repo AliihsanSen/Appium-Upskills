7370fb4033a37cd2ec566d6e6af4f1a42b35f334
Ali İhsan Şen <av.aliihsan1@gmail.com>
2024-01-18 14:26:49 +0300

    18.01.2024

--- src/test/java/upskills/day01/C01_SetupDriver.java
+++ src/test/java/upskills/day01/C01_SetupDriver.java
@@ -12,13 +12,21 @@ public class C01_SetupDriver {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","android");
         capabilities.setCapability("automationname","uiautomator2");
+        capabilities.setCapability("udid","emulator-5554");
     }
 
     @Test
     public void testUiAutomator2Options(){
 
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setPlatformName("android")
+        options.setUdid("emulator-5554"); // Tek bir cihaz var ise tanımlanmayabilir.
+
+        /**
+           options.setPlatformName("android")
                 .setAutomationName("uiautomator2");
+                // => Bu kısmı yorum satıra almamızın sebebi UiAutomator2Options'ta tanımlamış olduğumuz objede barındırıyor olması.
+         */
+
+
     }
 }
